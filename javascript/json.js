4b9f6f21f9dff348d15eaa59e6c8e1febee5624a
Simon Hoyos <shmesa23@gmail.com>
2020-08-06 11:11:03 -0500

    JSON

---
+++ javascript/json.js
@@ -0,0 +1,10 @@
+// JSON - JavaScript Object Notation
+{
+  "greet": "hola mundo",
+  "boolean": true,
+  "number": 1,
+  "array": ["string", true, 1, [], {}],
+  "object": {
+    "property": "string"
+  }
+}
