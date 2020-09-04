862fe88bbfd0fe98a14ff6258799e157a8af8bc2
Simon Hoyos <shmesa23@gmail.com>
2020-09-04 08:12:33 -0500

    Testing reducers

---
+++ testing/tasks/backend/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const db = require('./db');
+const userRouter = require('./routes/user');
+const taskRouter = require('./routes/task');
+
+const app = express();
+db();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use('/users', userRouter);
+app.use('/tasks', taskRouter);
+
+module.exports = app;
